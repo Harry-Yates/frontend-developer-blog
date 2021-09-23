7466da001e871eb4c4cc92c10116e6d8bfb6c544
Harry Yates <hyates1@gmail.com>
2021-09-23 10:55:25 +0200

    added a search bar to all posts page

--- js/script.js
+++ js/script.js
@@ -87,3 +87,18 @@ function getDrink() {
       populatePost(post);
     });
 }
+
+document.getElementById("searchInput").addEventListener("keyup", function (event) {
+  let searchQuery = event.target.value.toLowerCase();
+  let allNamesDOMCollection = document.getElementsByClassName("card-wrapper__card");
+
+  for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+    const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+
+    if (currentName.includes(searchQuery)) {
+      allNamesDOMCollection[counter].style.display = "block";
+    } else {
+      allNamesDOMCollection[counter].style.display = "none";
+    }
+  }
+});
